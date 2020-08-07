3c4812bbd57e9ca3cf8857b8ef3fd19ec5c70448
rboscan <51136553+rboscan@users.noreply.github.com>
2020-08-07 15:57:07 -0400

    Navbar Animation, Web Spacing

--- web-src/js/header.js
+++ web-src/js/header.js
@@ -1,7 +1,15 @@
-$(window).scroll(function() {
-    if($(this).scrollTop() > 300) {
-        $('.navbar navbar-expand-lg navbar-light').addClass('opaque');
+function checkScroll() {
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if ($(window).scrollTop() > startY) {
+        $('.navbar').addClass("scrolled");
     } else {
-        $('.navbar navbar-expand-lg navbar-light').removeClass('opaque');
+        $('.navbar').removeClass("scrolled");
     }
-});
+}
+
+if ($('.navbar').length > 0) {
+    $(window).on("scroll load resize", function () {
+        checkScroll();
+    });
+}
